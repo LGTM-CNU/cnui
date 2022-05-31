0b77fdd3f692dfb8051a43cc7537de2f3c083712
hyunjin <leehj0110@kakao.com>
2022-06-01 04:16:57 +0900

    feat: Text component, Storybook

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { css } from '@emotion/react';
 import { Text } from './Text';
 
 export default {
@@ -11,5 +12,106 @@ const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
 export const Basic = Template.bind({});
 
 Basic.args = {
-  children: 'Hello World',
+  children: 'Done is better than perfect. 완수하는 것이 완벽한 것 보다 낫다.',
+  size: 32,
+  weight: 500,
 };
+
+export function Size() {
+  const text =
+    'Done is better than perfect. 완수하는 것이 완벽한 것 보다 낫다.';
+
+  return (
+    <div css={wrapper}>
+      <Text size={72}>{text}</Text>
+      <Text size={56}>{text}</Text>
+      <Text size={48}>{text}</Text>
+      <Text size={40}>{text}</Text>
+      <Text size={32}>{text}</Text>
+      <Text size={24}>{text}</Text>
+      <Text size={20}>{text}</Text>
+      <Text size={16}>{text}</Text>
+      <Text size={14}>{text}</Text>
+      <Text size={12}>{text}</Text>
+      <Text size={10}>{text}</Text>
+    </div>
+  );
+}
+
+export function Weight() {
+  const text =
+    'Done is better than perfect. 완수하는 것이 완벽한 것 보다 낫다.';
+  return (
+    <div css={wrapper}>
+      <Text size={32} weight={900}>
+        {text}
+      </Text>
+      <Text size={32} weight={800}>
+        {text}
+      </Text>
+      <Text size={32} weight={700}>
+        {text}
+      </Text>
+      <Text size={32} weight={600}>
+        {text}
+      </Text>
+      <Text size={32} weight={500}>
+        {text}
+      </Text>
+      <Text size={32} weight={400}>
+        {text}
+      </Text>
+      <Text size={32} weight={300}>
+        {text}
+      </Text>
+      <Text size={32} weight={200}>
+        {text}
+      </Text>
+      <Text size={32} weight={100}>
+        {text}
+      </Text>
+    </div>
+  );
+}
+
+export function Align() {
+  const text = 'hyunjin';
+
+  return (
+    <div css={wrapper}>
+      <Text align="left">{text}</Text>
+      <Text align="center">{text}</Text>
+      <Text align="right">{text}</Text>
+    </div>
+  );
+}
+
+export function Truncate() {
+  const text =
+    'Done is better than perfect. 완수하는 것이 완벽한 것 보다 낫다.';
+  return (
+    <div css={[wrapper, { width: '150px' }]}>
+      <Text truncate>{text}</Text>
+    </div>
+  );
+}
+
+export function Clamp() {
+  const text =
+    'Done is better than perfect. 완수하는 것이 완벽한 것 보다 낫다.';
+  return (
+    <div css={[wrapper, { width: '150px' }]}>
+      <Text clamp={2}>{text}</Text>
+    </div>
+  );
+}
+
+const wrapper = css`
+  display: flex;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+  flex-direction: column;
+  & + & {
+    margin-top: 0.5rem;
+  }
+`;
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,4 +1,6 @@
 import { jsx, css } from '@emotion/react';
+import { useMemo } from 'react';
+import { ColorKey, cssVar } from '../../contexts';
 
 export interface TextProps {
   as?:
@@ -13,15 +15,66 @@ export interface TextProps {
     | 'div'
     | 'label'
     | 'strong';
+  whiteSpace?: 'pre' | 'nowrap' | 'pre-wrap' | 'pre-line' | 'normal';
+  align?: 'left' | 'center' | 'right';
+  size?: 10 | 12 | 14 | 16 | 20 | 24 | 32 | 40 | 48 | 56 | 72;
+  weight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
   children: React.ReactNode;
+  className?: string;
+  color?: ColorKey;
+  clamp?: number;
+  truncate?: boolean;
 }
 
-export function Text({ as = 'div', children }: TextProps) {
+export function Text({
+  as = 'div',
+  whiteSpace,
+  children,
+  size = 16,
+  weight = 400,
+  align,
+  className,
+  truncate,
+  clamp,
+  color,
+}: TextProps) {
+  const fontSize = useMemo(() => `${size / 16}rem`, [size]);
+
   return jsx(
     as,
     {
-      css: [css({})],
+      css: [
+        css({
+          whiteSpace,
+          fontSize,
+          fontWeight: weight,
+          textAlign: align,
+          color: color ? cssVar(color) : undefined,
+        }),
+        textStyle,
+        truncate && truncateStyle,
+        clamp && clampStyle(clamp),
+      ],
     },
     children
   );
 }
+
+const textStyle = css`
+  margin: 0;
+  line-height: 1.5;
+`;
+
+const truncateStyle = css`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 100%;
+`;
+
+const clampStyle = (lines: number) => css`
+  display: -webkit-box;
+  -webkit-line-clamp: ${lines};
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+`;
